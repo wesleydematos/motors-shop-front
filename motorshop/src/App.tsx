cf2da49a6e147079440579cb5aea822b34ae6a73
wesleydematos <wesleydematos3@gmail.com>
2023-04-10 14:58:20 -0300

    fix: update tailwind configs

--- motorshop/src/App.tsx
+++ motorshop/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Example from "./components/Example/example";
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <Example />
+    </div>
   );
 }
 

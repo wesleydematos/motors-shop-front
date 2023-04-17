27cc211d1299477ad33e63deeee532495f04e250
wesleydematos <wesleydematos3@gmail.com>
2023-04-17 18:10:36 -0300

    feat: create html to Registration Page

--- motorshop/src/pages/Register/index.tsx
+++ motorshop/src/pages/Register/index.tsx
@@ -1,115 +1,102 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
 export default function Register() {
   return (
-    <form className="w-full max-w-lg">
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-first-name"
-          >
-            First Name
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
-            type="text"
-            placeholder="Jane"
-          />
-          <p className="text-red-500 text-xs italic">
-            Please fill out this field.
-          </p>
+    <>
+      <div className="min-h-screen flex flex-col justify-between bg-grey-300">
+        <div>
+          <Header />
         </div>
-        <div className="w-full md:w-1/2 px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-last-name"
-          >
-            Last Name
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
-            type="text"
-            placeholder="Doe"
-          />
-        </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-password"
-          >
-            Password
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-password"
-            type="password"
-            placeholder="******************"
-          />
-          <p className="text-gray-600 text-xs italic">
-            Make it as long and as crazy as you'd like
-          </p>
-        </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-city"
-          >
-            City
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-city"
-            type="text"
-            placeholder="Albuquerque"
-          />
-        </div>
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-state"
-          >
-            State
-          </label>
-          <div className="relative">
-            <select
-              className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-state"
-            >
-              <option>New Mexico</option>
-              <option>Missouri</option>
-              <option>Texas</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
+        <main>
+          <form>
+            <h1>Cadastro</h1>
+
+            <p>Informações pessoais</p>
+
+            <label htmlFor="name">Nome</label>
+            <input type="text" id="name" placeholder="Ex: Vagner Love" />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Ex: example@mail.com" />
+
+            <label htmlFor="cpf">CPF</label>
+            <input type="text" id="cpf" placeholder="Ex: 000.000.000-00" />
+
+            <label htmlFor="phoneNumber">Celular</label>
+            <input
+              type="text"
+              id="phoneNumber"
+              placeholder="(DDD) 90000-0000"
+            />
+
+            <label htmlFor="dateBirth">Data de nascimento</label>
+            <input type="text" id="dateBirth" placeholder="00/00/00" />
+
+            <label htmlFor="description">Descrição</label>
+            <input
+              type="text"
+              id="description"
+              placeholder="Digitar descrição"
+            />
+
+            <p>Informações de endereço</p>
+
+            <label htmlFor="zipCode">CEP</label>
+            <input type="text" id="zipCode" placeholder="00000-000" />
+
+            <div>
+              <div>
+                <label htmlFor="state">Estado</label>
+                <input type="text" id="state" placeholder="Digitar Estado" />
+              </div>
+
+              <div>
+                <label htmlFor="city">CIdade</label>
+                <input type="text" id="city" placeholder="Digitar Cidade" />
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-zip"
-          >
-            Zip
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-zip"
-            type="text"
-            placeholder="90210"
-          />
-        </div>
+
+            <label htmlFor="street">Rua</label>
+            <input type="text" id="street" placeholder="Digitar rua" />
+
+            <div>
+              <div>
+                <label htmlFor="number">Numero</label>
+                <input type="text" id="number" placeholder="Digitar numero" />
+              </div>
+
+              <div>
+                <label htmlFor="complement">Complemento</label>
+                <input
+                  type="text"
+                  id="complement"
+                  placeholder="Ex: apartamento"
+                />
+              </div>
+            </div>
+
+            <label htmlFor="typeUser">Tipo de conta</label>
+            <select name="typeUser" id="typeUser">
+              <option value="buyer">Comprador</option>
+              <option value="advertiser">Anunciante</option>
+            </select>
+
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Digitar senha" />
+
+            <label htmlFor="passwordConf">Confirmar senha</label>
+            <input
+              type="password"
+              id="passwordConf"
+              placeholder="Digitar senha"
+            />
+
+            <button type="submit">Finalizar Cadastro</button>
+          </form>
+        </main>
+        <Footer />
       </div>
-    </form>
+    </>
   );
 }

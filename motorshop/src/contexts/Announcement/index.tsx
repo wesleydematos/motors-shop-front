ddbb84ecf4ac3c15055e030c8c05615389d82ed3
wesleydematos <wesleydematos3@gmail.com>
2023-04-26 15:48:13 -0300

    feat: make the home page dynamic

--- motorshop/src/contexts/Announcement/index.tsx
+++ motorshop/src/contexts/Announcement/index.tsx
@@ -1,5 +1,10 @@
-import { createContext, useContext } from "react";
-import { iAnnouncementContext, iAnnouncementContextProps } from "./types";
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../../services/api";
+import {
+  iAnnouncementContext,
+  iAnnouncementContextProps,
+  iCarResponse,
+} from "./types";
 
 const AnnouncementContext = createContext<iAnnouncementContext>(
   {} as iAnnouncementContext
@@ -8,8 +13,72 @@ const AnnouncementContext = createContext<iAnnouncementContext>(
 export const AnnouncementProvider = ({
   children,
 }: iAnnouncementContextProps) => {
+  const [cars, setCars] = useState([] as iCarResponse[]);
+  const [allCars, setAllCars] = useState([] as iCarResponse[]);
+  const [allBrands, setAllbrands] = useState([""]);
+  const [brandFil, setBrandFil] = useState([""]);
+  const [allModels, setAllModels] = useState([""]);
+  const [modelsFil, setModelsFil] = useState([""]);
+  const [colorsFil, setColorsFil] = useState([""]);
+  const [yearsFil, setYearsFil] = useState([] as number[]);
+  const [fuelsFil, setFuelsFil] = useState([""]);
+
+  async function getAllCars(): Promise<void> {
+    try {
+      const response = await api.get<iCarResponse[]>("/vehicles");
+      setCars(response.data);
+      setAllCars(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllCars();
+  }, []);
+
+  useEffect(() => {
+    const brands = allCars.map((car) => car.brand);
+
+    const uniqueBrands = brands.filter((item, index) => {
+      return brands.indexOf(item) === index;
+    });
+
+    setAllbrands(uniqueBrands);
+    setBrandFil(uniqueBrands);
+  }, [allCars]);
+
+  useEffect(() => {
+    const models = cars.map((car) => car.model);
+
+    const uniqueModels = models.filter((item, index) => {
+      return models.indexOf(item) === index;
+    });
+
+    setAllModels(uniqueModels);
+    setModelsFil(uniqueModels);
+  }, [cars]);
+
   return (
-    <AnnouncementContext.Provider value={{}}>
+    <AnnouncementContext.Provider
+      value={{
+        cars,
+        setCars,
+        brandFil,
+        setBrandFil,
+        modelsFil,
+        setModelsFil,
+        colorsFil,
+        setColorsFil,
+        yearsFil,
+        setYearsFil,
+        fuelsFil,
+        setFuelsFil,
+        allCars,
+        allBrands,
+        allModels,
+      }}
+    >
       {children}
     </AnnouncementContext.Provider>
   );
